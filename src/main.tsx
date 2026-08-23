38c0d188aa857096a3c48aa17263c6459eb1bf33
jxshua911 <joshuakasanga9@gmail.com>
2026-08-23 18:34:20 +0300

    Wire native OAuth listener into app startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { Capacitor } from '@capacitor/core';
+import { initNativeAuthListener } from './lib/nativeAuth';
+
+if (Capacitor.isNativePlatform()) {
+  initNativeAuthListener();
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
