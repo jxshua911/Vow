2e6c01892eb14e7798eaf660edb00427832ce555
jxshua911 <joshuakasanga9@gmail.com>
2026-08-31 12:49:32 +0300

    Register VOW icon customisation plugin

--- android/app/src/main/java/com/vow/app/MainActivity.java
+++ android/app/src/main/java/com/vow/app/MainActivity.java
@@ -2,4 +2,10 @@ package com.vow.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        registerPlugin(VowIconPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
