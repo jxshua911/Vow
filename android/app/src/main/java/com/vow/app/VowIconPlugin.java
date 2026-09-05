36c694a3e341cb26d753addf6ff7e2e99240803c
jxshua911 <joshuakasanga9@gmail.com>
2026-09-05 13:13:51 +0300

    fix: correct Capacitor PluginMethod import

--- android/app/src/main/java/com/vow/app/VowIconPlugin.java
+++ android/app/src/main/java/com/vow/app/VowIconPlugin.java
@@ -7,8 +7,8 @@ import android.content.pm.PackageManager;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
 
 @CapacitorPlugin(name = "VowIcon")
 public class VowIconPlugin extends Plugin {
