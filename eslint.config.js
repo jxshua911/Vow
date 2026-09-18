9667814494b3a67da3d215235ab1a8361a67f39c
jxshua911 <joshuakasanga9@gmail.com>
2026-09-18 10:12:02 +0300

    fix: ignore generated platform build files in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,16 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {
+    ignores: [
+      'dist/**',
+      'android/**',
+      'ios/**',
+      'components/**',
+      '.pnpm-store/**',
+      'node_modules/**',
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -24,5 +33,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  }
+  },
 );
