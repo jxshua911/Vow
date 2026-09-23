5c0456fd0deb731edf4127c88cc1892eb7e117fa
jxshua911 <joshuakasanga9@gmail.com>
2026-09-23 19:39:22 +0300

    Finish VOW public routes and account controls

---
+++ src/routes/privacy.tsx
@@ -0,0 +1,2 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+export const Route=createFileRoute("/privacy")({beforeLoad:()=>{throw redirect({to:"/privacy-policy",statusCode:301})}});
